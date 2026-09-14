bc32153b3b3e392562ed2d31c60fd2b49cc1f903
dinherr <dinhhenry@hotmail.com>
2026-09-13 23:07:12 -0700

    started foundation of home page + other sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,64 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import About from './About.jsx';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App(){
   return (
-
-    
       <section id="banner">
-        <div className="hero">
-          {/*
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-          */}
-        </div>
         <div>
+          <p>Hey, it's nice to meet you! I'm</p>
           <h1>Henry Dinh</h1>
           <p>
-            Hi there, I appreciate the visit. This is where you'll find more information about myself, my work, and<br />
-            the occassional hobby I partake in.
+            New graduate who's interested in crafting memorable UI/UX, <br></br>
+            experimenting with web designs, <br></br>
+            and developing convenient applications. 
           </p>
         </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
+
+        <div>
+          <h2> Work Experience </h2>
+          <p> 
+            Araam Sae Consulting             Client Support & Administrative Consultant <br></br>
+            Jan 2026 - Present
+          </p>
+          <ul>
+            <li> Actively communicated with clients to establish and iterate processes while supporting administrative responsibilities</li>
+          </ul>
+          <p>
+            Araam Sae Consulting             EA Support <br></br>
+            June 2026 - Present
+          </p>
+          <ul>
+            <li> Conducted fractional work with internal staff and assist founder with secretary tasks (reword this) </li>
+          </ul>
+          <p>
+            RW&CO.                           Sales Associate <br></br>
+            August 2022 - Jan 2026
+          </p>
+          <ul>
+            <li> Managed product inventory, drove sales for the store, and provided exceptional customer service</li>
+          </ul>
+          <p>
+            Truedan Bubble Tea               Back-of-House<br></br>
+            May 2021 - June 2022
+          </p>
+          <ul>
+            <li> Made all in-house toppings, prepared drinks, and tracked inventory and appliances</li>
+          </ul>
+        </div>
+
+        <div>
+          <h3> Education </h3>
+          <p>
+            Simon Fraser University         Bachelor's Degree in Computing Science <br></br>
+            September 2021 - April 2026     Specialized in Visual and Interactive Computing
+          </p>
+        </div>
       </section>
-  
-    
   )
     
 }
